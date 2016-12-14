d034386fed0cb8a1a9511183b62c3f1571ee9d7f
Start <shiqiaoshiqiao@163.com>
2016-12-14 15:48:35 +0800

    Initial Commit

---
+++ ColorPicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ColorPicker
+//
+//  Created by Start on 2016/12/14.
+//  Copyright © 2016年 Start. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ColorPicker/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ColorPicker
+//
+//  Created by Start on 2016/12/14.
+//  Copyright © 2016年 Start. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
