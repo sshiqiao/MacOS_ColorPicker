9ba45647f3dbdbb8a06309773bbe01975cde1f58
Qiao Shi <942850293@qq.com>
2018-08-30 17:47:33 +0800

    update

--- ColorPicker/AppDelegate.m
+++ ColorPicker/AppDelegate.m
@@ -14,7 +14,14 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     ColorPickerWindowController *colorPickerWindow = [[ColorPickerWindowController alloc] initWithWindowNibName:@"ColorPickerWindowController"];
     colorPickerWindow.selectedColorHandler = ^(Color *color){
-        NSLog(@"SelectedColor:#%@",color.HEXColorVaule);
+        NSLog(@"Hex Value:#%@",color.HEXColorVaule);
+        NSLog(@"H Value:%d",color.HSBColorVaule.H);
+        NSLog(@"S Value:%d",color.HSBColorVaule.S);
+        NSLog(@"B Value:%d",color.HSBColorVaule.B);
+        NSLog(@"R Value:%d",color.RGBColorVaule.R);
+        NSLog(@"G Value:%d",color.RGBColorVaule.G);
+        NSLog(@"B Value:%d",color.RGBColorVaule.B);
+        NSLog(@"A Value:%f",color.AValue);
     };
     [NSApp runModalForWindow:[colorPickerWindow window]];
 }
--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # MacOS_ColorPicker
 A custom colorPicker that runs on MacOS.
 
-![image](https://github.com/sshiqiao/MacOS_ColorPicker/blob/master/ColorPicker/screen_shoot.png)
+ColorPickerWindowController *colorPickerWindow = [[ColorPickerWindowController alloc] initWithWindowNibName:@"ColorPickerWindowController"];
+colorPickerWindow.selectedColorHandler = ^(Color *color){
+NSLog(@"Hex Value:#%@",color.HEXColorVaule);
+NSLog(@"H Value:%d",color.HSBColorVaule.H);
+NSLog(@"S Value:%d",color.HSBColorVaule.S);
+NSLog(@"B Value:%d",color.HSBColorVaule.B);
+NSLog(@"R Value:%d",color.RGBColorVaule.R);
+NSLog(@"G Value:%d",color.RGBColorVaule.G);
+NSLog(@"B Value:%d",color.RGBColorVaule.B);
+NSLog(@"A Value:%f",color.AValue);
+};
+
+![image](https://github.com/sshiqiao/MacOS_ColorPicker/blob/master/ColorPicker/demo_colorpicker.gif)
